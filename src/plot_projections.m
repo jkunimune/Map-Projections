90e9913a6ace2d4884aff40094f9f5c6c831e2c9
Justin Kunimune <justinkunimune@gmail.com>
2017-05-21 10:57:20 -1000

    Finished the Robinson Projection
    I fixed the Robinson Projection. I just used fewer values for the
    interpolation. It's still not perfect - you can see waves of distortion
    in the map analyzer where the polynomial interpolationis being wonky -
    but it's good enough for now.
    I also realized that by my metrics, Robinson is objectively better than
    Winkel Tripel. I thought there must be something wrong with my
    calculations, but upon closer inspection, I realized it's true. Winkel
    Tripel was adopted by NatGeo in 1998 because it "reduces the distortion
    of land masses as they near the poles," but in fact, Winkel Tripel makes
    the poles look even larger than Robinson does! Furthermore, while shapes
    near the poles are slightly better for Winkel Tripel, it severely skews
    landmasses near the Prime Meridian. NatGeo is just wrong, and I can
    prove it with math!
    What's more, I realized that now that I have this robust algorithm, I
    can use it to invent more map projections! I can optomize
    pseudocylindrical and tetrahedral projections to generate the most
    accurate map projections the world has ever seen. This is going to be
    fun.

--- src/plot_projections.m
+++ src/plot_projections.m
@@ -1,7 +1,7 @@
-Scale = [0.424,0.847,0.558,0.000,0.597,2.007,0.000,0.913,2.353,0.231,0.641,0.000,0.000,0.481,0.341];
-Shape = [0.215,0.000,0.157,0.344,0.391,0.000,0.500,0.750,0.750,0.318,0.122,0.393,0.421,0.000,0.157];
-Name = {' Equirectangular',sprintf('Mercator\n'),' Gall stereographic',' Hobo-Dyer',' Polar', sprintf('Stereographic\n'),' Azimuthal equal-area',' Orthographic','Gnomonic ',' Winkel tripel',' Van der Grinten',' Mollweide',' Hammer',sprintf('Pierce quincuncial\n'),'Tetragraph '};
-Align = {'left','left','left','left','left','left','left','left','right','left','left','left','left','right','right'};
+Scale = [0.424,0.847,0.558,0.000,0.597,2.007,0.000,0.913,2.353,0.231,0.641,0.000,0.000,0.481,0.341,0.207];
+Shape = [0.215,0.000,0.157,0.344,0.391,0.000,0.500,0.750,0.750,0.318,0.122,0.393,0.421,0.000,0.157,0.278];
+Name = {' Equirectangular',sprintf('Mercator\n'),' Gall stereographic',' Hobo-Dyer',' Polar', sprintf('Stereographic\n'),' Azimuthal equal-area',' Orthographic','Gnomonic ',' Winkel tripel',' Van der Grinten',' Mollweide',' Hammer',sprintf('Pierce quincuncial\n'),'Tetragraph ',' Robinson'};
+Align = {'left','left','left','left','left','left','left','left','right','left','left','left','left','right','right','left'};
 
 plot(Scale, Shape, '.', 'MarkerSize',20);
 xlabel('Size distortion');
