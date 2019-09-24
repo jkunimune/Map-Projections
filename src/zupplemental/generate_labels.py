3e819d9fdb992de7ab913a0d1acfb4b96d8c67d2
Justin Kunimune <justinkunimune@gmail.com>
2019-09-24 15:49:09 -0400

    It's supermap!
    I updated the supermap with some new city data I came across that makes it look way less lopsided than it did before. And then I fiddled with it a ton.

--- src/zupplemental/generate_labels.py
+++ src/zupplemental/generate_labels.py
@@ -9,7 +9,7 @@ from helpers import line_break, get_centroid
 def plot_texts(filename, label_class, max_rank, text_size, regulate_case=False, secondary_attr=None, force_points=False):
 	"""data from http://www.naturalearthdata.com/"""
 	sf = shapefile.Reader("../data/{}".format(filename))
-	lat_idx = None
+	rank_idx, type_idx, secd_idx, lat_idx, lon_idx = None, None, None, None, None
 	for i, field in enumerate(sf.fields):
 		if field[0] in ['scalerank', 'LABELRANK']:
 			rank_idx = i-1
@@ -24,7 +24,7 @@ def plot_texts(filename, label_class, max_rank, text_size, regulate_case=False, 
 		if field[0] == 'long_x':
 			lon_idx = i-1
 	for record, shape in zip(sf.records(), sf.shapes()):
-		rank = record[rank_idx]
+		rank = record[rank_idx] if rank_idx is not None else 0
 		if rank <= max_rank:
 			if lat_idx is not None:
 				x, y = record[lon_idx], record[lat_idx]
@@ -38,12 +38,12 @@ def plot_texts(filename, label_class, max_rank, text_size, regulate_case=False, 
 			if regulate_case:
 				label = label.upper()
 			if secondary_attr is not None and record[7] == 'Dependency':
-				label = "{} ({})".format(label, record[secd_idx]) #indicate dependencies
+				label = "{} ({})".format(label, record[secd_idx]) if secd_idx is not None else label #indicate dependencies
 			label_size = ['xs', 'sm', 'md', 'lg', 'xl'][max(0, int(text_size-rank))] #lower ranks get bigger text
 			print('\t<text class="label-{} label-{}" x="{:.03f}" y="{:.03f}">{}</text>'.format(
-					label_class, label_size, 180+x, 90-y, label))
-			if force_points or record[type_idx] in ['mountain', 'depression', 'pole', 'waterfall']: #add circles to the ones that need markers
-				print('\t<circle cx="{:.03f}" cy="{:.03f}" r="{:.03f}" />'.format(180+x, 90-y, math.sqrt(max_rank-rank+1)*0.15))
+					label_class, label_size, 180+x, 90-y, label.replace('&', '&amp;')))
+			if force_points or (type_idx is not None and record[type_idx] in ['mountain', 'depression', 'pole', 'waterfall']): #add circles to the ones that need markers
+				print('\t<circle cx="{:.03f}" cy="{:.03f}" r="{:.03f}" />'.format(180+x, 90-y, math.sqrt(text_size-rank+1)*0.15))
 
 def label_shapes(filename, label_class, max_rank=float('inf'), text_size=3):
 	plot_texts(filename, label_class, max_rank, text_size, secondary_attr='NOTE_ADM0')
--- src/zupplemental/generate_shape.py
+++ src/zupplemental/generate_shape.py
@@ -5,14 +5,19 @@ import shapefile
 from helpers import plot, trim_edges, lengthen_edges
 
 
-def plot_shapes(filename, max_rank=float('inf'), trim_antarctica=False, flesh_out_antarctica=False):
+def plot_shapes(filename, max_rank=float('inf'), clazz=None, trim_antarctica=False, flesh_out_antarctica=False, filter_field=None, filter_vals=['']):
 	"""data from http://www.naturalearthdata.com/"""
 	sf = shapefile.Reader("../data/{}".format(filename))
+	rank_idx, filter_idx = None, None
 	for i, field in enumerate(sf.fields):
 		if 'rank' in field[0]:
 			rank_idx = i-1
+		if field[0] == filter_field:
+			filter_idx = i-1
 	for record, shape in zip(sf.records(), sf.shapes()):
-		if record[rank_idx] <= max_rank:
+		if filter_idx is not None and record[filter_idx] not in filter_vals:
+			continue # skip it if it is filtered out
+		if rank_idx is None or record[rank_idx] <= max_rank:
 			if trim_antarctica:
 				if shape.points[0][1] < -60: #if it is Antarctica (this will have a few false positives, but that's fine)
 					shape.points = trim_edges(shape.points, shape.parts)
@@ -20,4 +25,4 @@ def plot_shapes(filename, max_rank=float('inf'), trim_antarctica=False, flesh_ou
 				if shape.points[0][1] < -60:
 					shape.points = lengthen_edges(shape.points)
 
-			plot(shape.points, midx=shape.parts, close=False, fourmat='xd')
+			plot(shape.points, midx=shape.parts, close=False, clazz=clazz, fourmat='xd')
