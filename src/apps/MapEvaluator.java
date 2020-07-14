e8dcf2b28fb0376d64c1a3a774eb8dc073cc97c4
Justin Kunimune <justinkunimune@gmail.com>
2020-07-14 09:16:38 -0400

    Scale is fake
    I added proper normalization for the areal distortion calculation.

--- src/apps/MapEvaluator.java
+++ src/apps/MapEvaluator.java
@@ -23,9 +23,11 @@
  */
 package apps;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import maps.Cylindrical;
 import maps.Gyorffy;
-import maps.Lenticular;
 import maps.Meshed;
 import maps.Polyhedral;
 import maps.Projection;
@@ -41,12 +43,11 @@ public class MapEvaluator {
 	
 	public static final double STEP = 1e-2;
 	
-	public static final double THRESHOLD = 2.0;
+	public static final double THRESHOLD = 3;
 	public static final Projection[] PROJECTIONS = {
-			Pseudocylindrical.SINUSOIDAL, Lenticular.VAN_DER_GRINTEN, Cylindrical.MERCATOR,
-//			Pseudocylindrical.MOLLWEIDE, Pseudocylindrical.ECKERT_IV,
-//			WinkelTripel.WINKEL_TRIPEL, Gyorffy.E, Gyorffy.F, Polyhedral.DYMAXION,
-//			Meshed.DANSEIJI_I, Meshed.DANSEIJI_II, Meshed.DANSEIJI_III,
+			Cylindrical.BEHRMANN, Pseudocylindrical.MOLLWEIDE, Pseudocylindrical.ECKERT_IV, Meshed.DANSEIJI_I,
+			WinkelTripel.WINKEL_TRIPEL, Gyorffy.E, Meshed.DANSEIJI_II,
+			Polyhedral.DYMAXION, Meshed.DANSEIJI_III,
 	};
 
 	/**
@@ -56,27 +57,32 @@ public class MapEvaluator {
 		for (Projection projection : PROJECTIONS) {
 			System.out.println(projection.getName());
 			
-			double totalArea = 0;
-			double shearedArea = 0, swollenArea = 0, unitArea = 0;
+			projection.setParameters(projection.getDefaultParameters());
+			
+			List<Double> arealDistortion = new ArrayList<Double>();
+			List<Double> angularDistortion = new ArrayList<Double>();
+			List<Double> weights = new ArrayList<Double>();
+			
 			double ɸStep = STEP;
 			for (double ɸMin = -Math.PI/2; ɸMin < Math.PI/2; ɸMin += ɸStep) {
 				double ɸMax = Math.min(ɸMin + ɸStep, Math.PI/2);
+				
 				double λStep = STEP/Math.cos((ɸMax + ɸMin)/2);
 				for (double λMin = -Math.PI; λMin < Math.PI; λMin += λStep) {
 					double λMax = Math.min(λMin + λStep, Math.PI);
-					
-					double dA = (Math.sin(ɸMax) - Math.sin(ɸMin))*(λMax - λMin);
 					
 					double[] n = projection.project((ɸMin + 3*ɸMax)/4, (λMin + λMax)/2);
 					double[] s = projection.project((3*ɸMin + ɸMax)/4, (λMin + λMax)/2);
 					double[] e = projection.project((ɸMin + ɸMax)/2, (λMin + 3*λMax)/4);
 					double[] w = projection.project((ɸMin + ɸMax)/2, (3*λMin + λMax)/4);
 					
-					double lengthScale = Math.sqrt(Math.abs(
-							(e[0] - w[0])*(n[1] - s[1]) - (n[0] - s[0])*(e[1] - w[1])));
+					double lengthScale = Math.sqrt(
+							(e[0] - w[0])*(n[1] - s[1]) - (n[0] - s[0])*(e[1] - w[1]));
 					double nonlinearity = Math.hypot(
 							n[0] + s[0] - e[0] - w[0], n[1] + s[1] - e[1] - w[1])/lengthScale;
-					if (nonlinearity <= 1) { // only count distortion measurements where the map is smooth
+					if (nonlinearity < 2) { // only count distortion measurements where the map is smooth
+						double dA = (Math.sin(ɸMax) - Math.sin(ɸMin))*(λMax - λMin);
+						
 						double dxdɸ = (n[0] - s[0])/((ɸMax - ɸMin)/2);
 						double dxdλ = (e[0] - w[0])/((λMax - λMin)/2);
 						double dydɸ = (n[1] - s[1])/((ɸMax - ɸMin)/2);
@@ -86,30 +92,46 @@ public class MapEvaluator {
 						
 						double s1ps2 = Math.hypot(dxdλ/dsdλ + dydɸ/dsdɸ, dydλ/dsdλ - dxdɸ/dsdɸ);
 						double s1ms2 = Math.hypot(dxdλ/dsdλ - dydɸ/dsdɸ, dydλ/dsdλ + dxdɸ/dsdɸ);
-						double angularDistortion = (s1ps2 - s1ms2)/(s1ps2 + s1ms2);
-						double arealDistortion = (dxdλ*dydɸ - dxdɸ*dydλ)/dsdɸ/dsdλ;
-						
-						if (angularDistortion < 1/THRESHOLD || angularDistortion > THRESHOLD)
-							shearedArea += dA;
-						if (arealDistortion < 1/THRESHOLD || arealDistortion > THRESHOLD)
-							swollenArea += dA;
-						if (unitArea < dA)
-							unitArea = dA;
-						totalArea += dA;
+						angularDistortion.add((s1ps2 - s1ms2)/(s1ps2 + s1ms2));
+						arealDistortion.add((dxdλ*dydɸ - dxdɸ*dydλ)/dsdɸ/dsdλ);
+						weights.add(dA);
 					}
 					else {
-						System.out.printf("[%f,%f],[%f,%f],[%f,%f],[%f,%f]\n", n[0], n[1], e[0], e[1], s[0], s[1], w[0], w[1]);
-						System.out.println("Nonlinearity detected at "+ɸMin+"N, "+λMin+"E: "+nonlinearity);
+//						System.out.printf("[%f,%f],[%f,%f],[%f,%f],[%f,%f]\n", n[0], n[1], e[0], e[1], s[0], s[1], w[0], w[1]);
+//						System.out.println("Nonlinearity detected at "+ɸMin+"N, "+λMin+"E: "+nonlinearity);
 					}
 				}
 			}
 			
-			System.out.printf("Usable area:%.6f sr\n", totalArea);
+			double minScale = Double.POSITIVE_INFINITY, maxArea = Double.NEGATIVE_INFINITY;
+			for (int i = 0; i < weights.size(); i ++) {
+				if (arealDistortion.get(i) < minScale)
+					minScale = arealDistortion.get(i);
+				if (weights.get(i) > maxArea)
+					maxArea = weights.get(i);
+			}
+			double shearedArea = 0, swollenArea = 0, totalArea = 0;
+			for (int i = 0; i < weights.size(); i ++) {
+				if (absoluteQuality(angularDistortion.get(i)) > THRESHOLD)
+					shearedArea += weights.get(i);
+				if (arealDistortion.get(i) - minScale > THRESHOLD)
+					swollenArea += weights.get(i);
+				totalArea += weights.get(i);
+			}
+			
+			System.out.printf("Usable area: %.6f sr\n", totalArea);
 			System.out.printf("Anglular:   %8.4f %%\n", 100*shearedArea/totalArea);
 			System.out.printf("Areal:      %8.4f %%\n", 100*swollenArea/totalArea);
-			System.out.printf("Precision:  %8.4f %%\n", 100*unitArea/totalArea);
+			System.out.printf("Precision:  %8.4f %%\n", 100*maxArea/totalArea);
 			System.out.println();
 		}
+	}
+	
+	private static final double absoluteQuality(double x) {
+		if (Math.abs(x) < 1)
+			return 1/x;
+		else
+			return x;
 	}
 
 }
