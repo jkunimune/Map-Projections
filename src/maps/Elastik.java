b04f756b48091f61d4de8e2f40b241288a2973f4
Justin Kunimune <justinkunimune@gmail.com>
2023-06-14 23:01:23 -0700

    exact inverse solution
    the edges of the mesh are a little crunchy now, which is weird because I thaut I set the termination condition to prevent that, but overall this looks quite good.  I hope it's correct.

--- src/maps/Elastik.java
+++ src/maps/Elastik.java
@@ -44,6 +44,7 @@ import static java.lang.Math.cos;
 import static java.lang.Math.floor;
 import static java.lang.Math.hypot;
 import static java.lang.Math.min;
+import static java.lang.Math.signum;
 import static java.lang.Math.sin;
 import static java.lang.Math.toRadians;
 
@@ -97,9 +98,9 @@ public class Elastik {
 
 		/**
 		 * convert a location on the globe to a location on the map plane
-		 * @param ф the latitude in radians
-		 * @param λ the longitude in radians
-		 * @return the x value and y value in the same units as this.width and this.height
+		 * @param ф the latitude, in radians
+		 * @param λ the longitude, in radians
+		 * @return the x value and y value, in the same units as this.width and this.height
 		 */
 		public double[] project(double ф, double λ) {
 			for (int i = 0; i < sections.length; i ++) {
@@ -116,16 +117,43 @@ public class Elastik {
 
 		/**
 		 * convert a location on the map plane to a location on the globe
-		 * @param x the x value in the same units as this.width
-		 * @param y the y value in the same units as this.height
-		 * @return the latitude and longitude in radians
+		 * @param x the x value, in the same units as this.width
+		 * @param y the y value, in the same units as this.height
+		 * @return the latitude and longitude, in radians, or null if the point is not on the mesh
 		 */
 		public double[] inverse(double x, double y) {
+			// start by interpolating on the raster
+			double[] guess = inverse_by_interpolation(x, y);
+			// then use Newton-Raphson iteration to arrive at an exact solution for each section
+			double[][] exact_solutions = new double[sections.length][];
+			for (int i = 0; i < sections.length; i ++) {
+				exact_solutions[i] = inverse_by_iteration(x, y, i, guess);
+				// if any solution is non-null and also in-bounds, use it immediately
+				if (exact_solutions[i] != null &&
+				    section_borders[i].contains(exact_solutions[i][0], exact_solutions[i][1]))
+					return exact_solutions[i];
+			}
+			// otherwise, arbitrarily choose one of the non-null solutions
+			for (int i = 0; i < sections.length; i ++)
+				if (exact_solutions[i] != null)
+					return exact_solutions[i];
+			// if no solutions are non-null, shikatanai.
+			return null;
+		}
+
+		/**
+		 * convert a location on the map plane to an approximate location on the globe by
+		 * bilinearly interpolating on the inverse raster
+		 * @param x the x value, in the same units as this.width
+		 * @param y the y value, in the same units as this.height
+		 * @return the latitude and longitude, in radians
+		 */
+		private double[] inverse_by_interpolation(double x, double y) {
 			// find the correct bin
 			double i_partial = (y - raster_lower)/raster_height*(inverse_raster.length - 1);
-			int i = (int)floor(min(i_partial, inverse_raster.length - 2));
+			int i = (int) floor(min(i_partial, inverse_raster.length - 2));
 			double j_partial = (x - raster_left)/raster_width*(inverse_raster[i].length - 1);
-			int j = (int)floor(min(j_partial, inverse_raster[i].length - 2));
+			int j = (int) floor(min(j_partial, inverse_raster[i].length - 2));
 
 			// calculate the linear weights
 			double right_weight = j_partial - j;
@@ -135,17 +163,72 @@ public class Elastik {
 
 			// perform the interpolation on our 3D cartesian inverse raster
 			double[] result = new double[3];
-			for (int l = 0; l < 3; l ++) {
+			for (int l = 0; l < 3; l++) {
 				result[l] = left_weight*(lower_weight*inverse_raster[i][j][l] +
 				                         upper_weight*inverse_raster[i + 1][j][l]) +
 				            right_weight*(lower_weight*inverse_raster[i][j + 1][l] +
 				                          upper_weight*inverse_raster[i + 1][j + 1][l]);
 			}
 
-			// convert to spherical coordinates and return
+			// convert to spherical coordinates
 			return new double[] {
 					atan(result[2]/hypot(result[0], result[1])),
-			        atan2(result[1], result[0]) };
+					atan2(result[1], result[0])};
+		}
+
+		/**
+		 * convert a location on the map plane to a location on the globe by iteratively searching
+		 * for the coordinates that project to x and y on a given section
+		 * @param x the x value, in the same units as this.width
+		 * @param y the y value, in the same units as this.height
+		 * @param i the index of the section to search
+		 * @param guess an initial input that projects to the correct vicinity
+		 * @return the latitude and longitude, in radians, or null if no solution can be found
+		 */
+		private double[] inverse_by_iteration(double x, double y, int i, double[] guess) {
+			final double step_size = 1e-4;
+			final double tolerance = 1e-2;
+			final double max_iterations = 100;
+			// until we find the solution...
+			double previous_distance = POSITIVE_INFINITY;
+			int num_iterations = 0;
+			while (true) {
+				// evaluate the projection at the current guess
+				double[] center_value = new double[2];
+				for (int l = 0; l < 2; l ++)
+					center_value[l] = sections[i][l].evaluate(guess[0], guess[1]);
+
+				// check the stopping conditions
+				double distance = hypot(center_value[0] - x, center_value[1] - y);
+				if (distance < tolerance)
+					return guess;  // solution is found
+				if (isNaN(distance) || distance > previous_distance)
+					return null;  // solution is not converging
+				if (num_iterations > max_iterations)
+					return null;  // too many iterations have elapsed
+				previous_distance = distance;
+				num_iterations ++;
+
+				// evaluate the jacobian using finite differences
+				double[][] jacobian = new double[2][2];
+				for (int l = 0; l < 2; l ++) {
+					double north_value = sections[i][l].evaluate(guess[0] + step_size, guess[1]);
+					double east_value = sections[i][l].evaluate(guess[0], guess[1] + step_size);
+					jacobian[l][0] = (north_value - center_value[l])/step_size;
+					jacobian[l][1] = (east_value - center_value[l])/step_size;
+				}
+
+				// calculate the step
+				double[] residual = {center_value[0] - x, center_value[1] - y};
+				double[] step = solve_linear_equation(jacobian, residual);
+				// take the step
+				guess[0] -= step[0];
+				guess[1] -= step[1];
+				guess[0] = guess[0] - floor((guess[0] + PI)/(2*PI))*(2*PI);  // coerce it back into the valid domain
+				if (abs(guess[0]) > PI/2)
+					guess[0] = signum(guess[0])*(PI/2 - abs(guess[0]));
+				guess[1] = guess[1] - floor((guess[1] + PI)/(2*PI))*(2*PI);
+			}
 		}
 
 
@@ -304,27 +387,6 @@ public class Elastik {
 
 
 	/**
-	 * average two gradients at two locations in a stack of spline surfaces so that the splines are
-	 * C^1 continuous at that point.
-	 */
-	private static void set_gradients_equal(
-			SplineSurface[][] surfaces, int i_A, int j_A, int k_A, int i_B, int j_B, int k_B) {
-		for (int l = 0; l < 2; l ++) {
-			double mean_gradient_dф = (
-					surfaces[i_A][l].gradients_dф[j_A][k_A] +
-					surfaces[i_B][l].gradients_dф[j_B][k_B])/2;
-			surfaces[i_A][l].gradients_dф[j_A][k_A] = mean_gradient_dф;
-			surfaces[i_B][l].gradients_dф[j_B][k_B] = mean_gradient_dф;
-			double mean_gradient_dλ = (
-					surfaces[i_A][l].gradients_dλ[j_A][k_A] +
-					surfaces[i_B][l].gradients_dλ[j_B][k_B])/2;
-			surfaces[i_A][l].gradients_dλ[j_A][k_A] = mean_gradient_dλ;
-			surfaces[i_B][l].gradients_dλ[j_B][k_B] = mean_gradient_dλ;
-		}
-	}
-
-
-	/**
 	 * a polygon on a globe with a function for determining if a point is contained by it or not.
 	 * the points are assumed to go counterclockwise, so if a point sees the vertices going left,
 	 * that point is contained.
@@ -469,6 +531,12 @@ public class Elastik {
 			}
 		}
 
+		/**
+		 * interpolate this function to the given coordinates
+		 * @param ф the input that moves along the zeroth axis of values
+		 * @param λ the input that moves along the first axis of values
+		 * @return the value of the spline
+		 */
 		public double evaluate(double ф, double λ) {
 			double i_partial = (ф + PI/2)/PI*(values.length - 1);
 			int i = (int)floor(min(i_partial, values.length - 2));
@@ -478,27 +546,75 @@ public class Elastik {
 			    isNaN(values[i + 1][j]) || isNaN(values[i + 1][j + 1]))
 				return NaN;
 
-			double z_west = hermiteSpline1D(values[i][j], gradients_dф[i][j],
-			                                values[i + 1][j], gradients_dф[i + 1][j],
+			double z_west = hermite_spline_1D(values[i][j], gradients_dф[i][j],
+			                                  values[i + 1][j], gradients_dф[i + 1][j],
 			                                i_partial - i);
-			double dzdλ_west = hermiteSpline1D(gradients_dλ[i][j], 0.,
-			                                   gradients_dλ[i + 1][j], 0.,
+			double dzdλ_west = hermite_spline_1D(gradients_dλ[i][j], 0.,
+			                                     gradients_dλ[i + 1][j], 0.,
 			                                   i_partial - i);
-			double z_east = hermiteSpline1D(values[i][j + 1], gradients_dф[i][j + 1],
-			                                values[i + 1][j + 1], gradients_dф[i + 1][j + 1],
+			double z_east = hermite_spline_1D(values[i][j + 1], gradients_dф[i][j + 1],
+			                                  values[i + 1][j + 1], gradients_dф[i + 1][j + 1],
 			                                i_partial - i);
-			double dzdλ_east = hermiteSpline1D(gradients_dλ[i][j + 1], 0.,
-			                                   gradients_dλ[i + 1][j + 1], 0.,
+			double dzdλ_east = hermite_spline_1D(gradients_dλ[i][j + 1], 0.,
+			                                     gradients_dλ[i + 1][j + 1], 0.,
 			                                   i_partial - i);
-			return hermiteSpline1D(z_west, dzdλ_west, z_east, dzdλ_east,
+			return hermite_spline_1D(z_west, dzdλ_west, z_east, dzdλ_east,
 			                       j_partial - j);
 		}
 
-		private static double hermiteSpline1D(double y_0, double dydx_0, double y_1, double dydx_1, double x) {
-			return y_0 +
-			       x*(dydx_0 +
-			          x*((3*(y_1 - y_0) - 2*dydx_0 - dydx_1) +
-			             x*(dydx_0 + dydx_1 - 2*(y_1 - y_0))));
+	}
+
+
+	/**
+	 * a polynomial with specified values and derivatives at two endpoints
+	 * @param y_0 the value of the function at x=0
+	 * @param dydx_0 the slope of the function at x=0
+	 * @param y_1 the value of the function at x=1
+	 * @param dydx_1 the slope of the function at x=1
+	 * @param x the dimensionless function input in the range [0, 1]
+	 * @return a smoothly varying value somewhere in the vicinity of y_0 and y_1
+	 */
+	private static double hermite_spline_1D(double y_0, double dydx_0, double y_1, double dydx_1, double x) {
+		return y_0 +
+		       x*(dydx_0 +
+		          x*((3*(y_1 - y_0) - 2*dydx_0 - dydx_1) +
+		             x*(dydx_0 + dydx_1 - 2*(y_1 - y_0))));
+	}
+
+
+	/**
+	 * average two gradients at two locations in a stack of spline surfaces so that the splines are
+	 * C^1 continuous at that point.
+	 */
+	private static void set_gradients_equal(
+			SplineSurface[][] surfaces, int i_A, int j_A, int k_A, int i_B, int j_B, int k_B) {
+		for (int l = 0; l < 2; l ++) {
+			double mean_gradient_dф = (
+					                          surfaces[i_A][l].gradients_dф[j_A][k_A] +
+					                          surfaces[i_B][l].gradients_dф[j_B][k_B])/2;
+			surfaces[i_A][l].gradients_dф[j_A][k_A] = mean_gradient_dф;
+			surfaces[i_B][l].gradients_dф[j_B][k_B] = mean_gradient_dф;
+			double mean_gradient_dλ = (
+					                          surfaces[i_A][l].gradients_dλ[j_A][k_A] +
+					                          surfaces[i_B][l].gradients_dλ[j_B][k_B])/2;
+			surfaces[i_A][l].gradients_dλ[j_A][k_A] = mean_gradient_dλ;
+			surfaces[i_B][l].gradients_dλ[j_B][k_B] = mean_gradient_dλ;
 		}
+	}
+
+
+	/**
+	 * find the vector x such that Ax = b.  this function only works on 2d systems.
+	 * @param A a 2×2 matrix to be inverted and multiplied
+	 * @param b a 2-vector to be multiplied by the inverse of A
+	 * @return the 2-vector x that solves the linear equation Ax = b
+	 */
+	private static double[] solve_linear_equation(double[][] A, double[] b) {
+		if (A.length != 2 || b.length != 2)
+			throw new IllegalArgumentException("this function has only been implemented for 2D equations.");
+		double det = A[0][0]*A[1][1] - A[0][1]*A[1][0];
+		return new double[] {
+				(A[1][1]*b[0] - A[0][1]*b[1])/det,
+				(A[0][0]*b[1] - A[1][0]*b[0])/det };
 	}
 }
