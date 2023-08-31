43a5799e2f26b11b9c11816243043a9d366e2e26
Justin Kunimune <justinkunimune@gmail.com>
2023-08-31 07:50:06 -0400

    simplify label placement code
    this was silly.

--- src/zupplemental/generate_labels.py
+++ src/zupplemental/generate_labels.py
@@ -1,55 +1,59 @@
-#read data from a shapefile into SVG format
-
-import math
+# read data from a shapefile into SVG format
 
 import shapefile
+from numpy import sqrt
 
 from helpers import get_centroid
 
 
-def plot_texts(filename, label_class, max_rank, text_size, regulate_case=False, secondary_attr=None, force_points=False) -> str:
-	"""data from http://www.naturalearthdata.com/"""
+def plot_texts(
+		filename, label_class, max_rank, text_size,
+		regulate_case=False, secondary_attr=None, force_points=False) -> str:
+	"""data from https://www.naturalearthdata.com/"""
 	try:
 		sf = shapefile.Reader("shapefiles/{}".format(filename))
 	except shapefile.ShapefileException:
 		raise FileNotFoundError(f"The shapefile {filename} is missing; please download it and put it in "
 		                        f"src/zupplemental/shapefiles/")
-	rank_idx, name_idx, type_idx, secd_idx, lat_idx, lon_idx = None, None, None, None, None, None
+	secd_idx = None
 	for i, field in enumerate(sf.fields):
-		if field[0] in ['scalerank', 'LABELRANK']:
-			rank_idx = i-1
-		if field[0] in ['name', 'NAME']:
-			name_idx = i-1
-		if field[0] == 'featurecla':
-			type_idx = i-1
 		if field[0] == secondary_attr:
-			secd_idx = i-1
-		if field[0] == 'lat_y':
-			lat_idx = i-1
-		if field[0] == 'long_x':
-			lon_idx = i-1
+			secd_idx = i-1  # find the secondary attribute index
+
 	result = ""
-	for record, shape in zip(sf.records(), sf.shapes()):
-		rank = record[rank_idx] if rank_idx is not None else 0
-		if rank <= max_rank:
-			if lat_idx is not None:
-				x, y = record[lon_idx], record[lat_idx]
-			else:
-				x, y = get_centroid(shape.points, shape.parts)
-			label = record[name_idx]
-			if not label:
-				continue #we can't waste our time worrying about features with no names
-			if label == 'EUROPE':
-				rank += 1 #You're not a continent! Get over it!
-			if regulate_case:
-				label = label.upper()
-			if secondary_attr is not None and record[7] == 'Dependency':
-				label = "{} ({})".format(label, record[secd_idx]) if secd_idx is not None else label #indicate dependencies
-			label_size = ['xs', 'sm', 'md', 'lg', 'xl'][max(0, int(text_size-rank))] #lower ranks get bigger text
-			result += '\t<text class="label-{} label-{}" x="{:.03f}" y="{:.03f}">{}</text>\n'.format(
-					label_class, label_size, 180+x, 90-y, label.replace('&', '&amp;'))
-			if force_points or (type_idx is not None and record[type_idx] in ['mountain', 'depression', 'pole', 'waterfall']): #add circles to the ones that need markers
-				result += '\t<circle cx="{:.03f}" cy="{:.03f}" r="{:.03f}" />\n'.format(180+x, 90-y, math.sqrt(text_size-rank+1)*0.15)
+	for region in sf.shapeRecords():
+		try:
+			rank = region.record.LABELRANK
+		except AttributeError:
+			rank = 0
+		if rank > max_rank:
+			continue  # skip anything too obscure to note
+		label = region.record.NAME
+		if not label:
+			continue  # we can't waste our time worrying about features with no names
+		try:
+			x, y = region.record.long_x, region.record.lat_y
+		except AttributeError:
+			x, y = get_centroid(region.shape.points, region.shape.parts)
+		try:
+			tipe = region.record.TYPE
+		except AttributeError:
+			tipe = "thing"
+
+		if region.record.NAME == 'EUROPE':
+			rank += 1  # You're not a continent! Get over it!
+		if regulate_case:
+			label = region.record.NAME.upper()
+		if secondary_attr is not None and tipe == 'Dependency':
+			label = f"{region.record.NAME} ({region.record[secd_idx]})"  # indicate dependencies' sovereigns in parentheses
+		label = label.replace("&", "&amp;")
+		label_size = ['xs', 'sm', 'md', 'lg', 'xl'][max(0, int(text_size-rank))]  # lower ranks get bigger text
+
+		# create the label as a <text> tag
+		result += f'\t<text class="label-{label_class} label-{label_size}" x="{180+x:.03f}" y="{90-y:.03f}">{label}</text>\n'
+		# add circles to the ones that need markers
+		if force_points or (region.record.featurecla in ['mountain', 'depression', 'pole', 'waterfall']):
+			result += f'\t<circle cx="{180+x:.03f}" cy="{90-y:.03f}" r="{sqrt(text_size-rank+1)*0.15:.03f}" />\n'
 	return result
 
 def label_shapes(filename, label_class, max_rank=float('inf'), text_size=3) -> str:
