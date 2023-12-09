ef37eb6ddd2b92c7c3d20e3e4887195a29300fb6
Justin Kunimune <justinkunimune@gmail.com>
2023-12-09 14:36:26 -0500

    reduce number of color bins

--- src/zupplemental/colorcode_countries.py
+++ src/zupplemental/colorcode_countries.py
@@ -310,7 +310,7 @@ COUNTRIES_BY_AREA = {
 	"SER": 1200,
 }
 
-density_bins = array([.02, 10, 20, 40, 80, 150, 300, 600, 1_200, 2_500])
+density_bins = array([.02, 10, 20, 50, 100, 200, 500, 1000, 2000])
 
 swatches = stack(
 	[
